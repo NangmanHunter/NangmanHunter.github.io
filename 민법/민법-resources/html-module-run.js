0562af8b0c1486df344d239fd577e5426faf2a5a
NangmanHunter <djwlfdls1@naver.com>
2024-11-27 09:09:30 +0900

    feat: 동기착오

--- 민법/민법-resources/html-module-run.js
+++ 민법/민법-resources/html-module-run.js
@@ -5,6 +5,7 @@ async function run() {
     $(`body`).append(` <script src="민법-resources/html-module-2.js"></script>`);
     await new Promise(resolve => setTimeout(resolve, 100)); 
     $(`body`).append(`<script src="민법-resources/html-module-어휘.js"></script>`);
+    $(`body`).append(`<script src="민법-resources/a.js"></script>`);
 
     // await new Promise(resolve => setTimeout(resolve, 100)); 
 
